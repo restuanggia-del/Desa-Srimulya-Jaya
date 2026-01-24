f0ba07f6f738d5cc603b10ae114b8bfddef8f694
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-01-24 21:24:06 +0700

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-const isProd = process.env.NODE_ENV === 'production';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://restuanggia-del.github.io',
-  base: isProd ? '/Desa-Srimulya-Jaya/' : '/',
 });
